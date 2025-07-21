e04ffd1b685bcb6928026e419ad9b84d13554109
DON MASS <aloysius.mirel96@gmail.com>
2025-07-21 15:24:02 +0700

    file download

--- js/Data/file_download_data.js
+++ js/Data/file_download_data.js
@@ -1,17 +1,17 @@
  // 📦 Data URL untuk tombol download
   const downloadData = [
-    { id: "file_1", url: "https://drive.google.com/drive/folders/1Z4tG3FMdMa3oZ3Nk9oqAp8ydWOlFcDS8?usp=drive_link" },
-    { id: "file_2", url: "https://www.dropbox.com/s/example123/file.zip?dl=1" },
-    { id: "file_3", url: "https://example.com/files/output.zip" },
-    { id: "file_4", url: "https://example.com/files/output.zip" },
-    { id: "file_5", url: "https://example.com/files/output.zip" },
-    { id: "file_6", url: "https://example.com/files/output.zip" },
-    { id: "file_7", url: "https://example.com/files/output.zip" },
-    { id: "file_8", url: "https://example.com/files/output.zip" },
-    { id: "file_9", url: "https://example.com/files/output.zip" },
-    { id: "file_10", url: "https://example.com/files/output.zip" },
-    { id: "file_11", url: "https://example.com/files/output.zip" },
-    { id: "file_12", url: "https://example.com/files/output.zip" },
-    { id: "file_13", url: "https://example.com/files/output.zip" },
-    { id: "file_14", url: "https://example.com/files/output.zip" }
+    { id: "file_1", url: "https://drive.google.com/drive/folders/1iB5H-FK8tgsuqzp9W-4CSBR4-lYvnyzO?usp=sharing" },
+    { id: "file_2", url: "https://drive.google.com/drive/folders/1AI8cc9QrNpdyJIbGy5znXOgQfJgAvci3?usp=sharing" },
+    { id: "file_3", url: "https://drive.google.com/drive/folders/1yPgNB1ugKi85xK2_096BSwU0uAz0E5E_?usp=sharing" },
+    { id: "file_4", url: "https://drive.google.com/drive/folders/1SLGd88MVbMuefzrxuUshvkSFpZGYTM53?usp=sharing" },
+    { id: "file_5", url: "https://drive.google.com/drive/folders/1X_2xUz08lZXMBxUUfaWfLjf9XHCYrX2o?usp=sharing" },
+    { id: "file_6", url: "https://drive.google.com/drive/folders/1kEJeUr_sZIRtKNZ-muto5fTVpn_rGOrO?usp=sharing" },
+    { id: "file_7", url: "https://drive.google.com/drive/folders/1YSRbICDbQzKKo8iXKLWIP0gNBd1BDGic?usp=sharing" },
+    { id: "file_8", url: "https://drive.google.com/drive/folders/1JU-dEsBxWVcmvH-51EQUnJv-LSvOBvbm?usp=sharing" },
+    { id: "file_9", url: "https://drive.google.com/drive/folders/1-8eyppSolqLrNWwrfNEr6dYoeWZJ-Nkc?usp=sharing" },
+    { id: "file_10", url: "https://drive.google.com/drive/folders/10U4SEHCouNqAQgYjdoOP9Ifn7qIl6Zrg?usp=sharing" },
+    { id: "file_11", url: "https://drive.google.com/drive/folders/1MTN-m7spnWTHDZw3ZnUZ1Gu4IjHLBK3o?usp=sharing" },
+    { id: "file_12", url: "https://drive.google.com/drive/folders/1pVOwzc9EKALFFGBtSJjihmBk6PB_CqpY?usp=sharing" },
+    { id: "file_13", url: "https://drive.google.com/drive/folders/1xhpW7k3e6m5r73iZ9-58uYlcPcsU0hpF?usp=sharing" },
+    { id: "file_14", url: "https://drive.google.com/drive/folders/1gBsaBycrPHULoduJd41Iu2FiQklWGjds?usp=sharing" }
   ];
